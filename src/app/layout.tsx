d60c54c0aa485acfce5e7ab68efd906af0ab4c58
Abhishek Goel <abhishek.goel287@gmail.com>
2025-10-17 23:55:13 +0530

    updated ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/porta_logo.png" type="image/png" />
+        <link rel="shortcut icon" href="/porta_logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/porta_logo.png" />
+      </head>
       <body
         className={`${outfit.variable} ${playfair.variable} ${inter.variable} antialiased`}
       >
